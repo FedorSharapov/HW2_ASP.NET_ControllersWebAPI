fd694a747712beaad58b3716a260cbbfa725b532
Федор Шарапов <sharapovfedor@yandex.ru>
2023-02-24 18:25:24 +0100

    добавлены реализации Create/Delete/Update в репозиторий

--- src/Otus.Teaching.PromoCodeFactory.DataAccess/Repositories/InMemoryRepository.cs
+++ src/Otus.Teaching.PromoCodeFactory.DataAccess/Repositories/InMemoryRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Application.Common.Exceptions;
 using Otus.Teaching.PromoCodeFactory.Core.Domain;
 
 namespace Otus.Teaching.PromoCodeFactory.DataAccess.Repositories
@@ -25,7 +26,39 @@ namespace Otus.Teaching.PromoCodeFactory.DataAccess.Repositories
 
         public Task<T> GetByIdAsync(Guid id)
         {
-            return Task.FromResult(Data.FirstOrDefault(x => x.Id == id));
+            var entity = Data.FirstOrDefault(e => e.Id == id);
+
+            if (entity == null)
+                throw new NotFoundException(typeof(T).Name, id);
+
+            return Task.FromResult(entity);
+        }
+
+        public Task<Guid> CreateAsync(T entity)
+        {
+            entity.Id = Guid.NewGuid();
+            Data = Data.Concat(new[] { entity });
+
+            return Task.FromResult(entity.Id);
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            if (Data.FirstOrDefault(e => e.Id == entity.Id) == null)
+                throw new NotFoundException(typeof(T).Name, entity.Id);
+
+            Data = Data.Where(e => e.Id != entity.Id);
+            Data = Data.Concat(new[] { entity });
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            if (Data.FirstOrDefault(e => e.Id == id) == null)
+                throw new NotFoundException(typeof(T).Name, id);
+
+            Data = Data.Where(e => e.Id != id);
+            return Task.CompletedTask;
         }
     }
 }
